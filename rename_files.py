874dfa291025b3f6e51da954d6e45dd5fe9fc17a
André Ferreira <contato@andreferreira.me>
2017-09-18 15:34:39 -0300

    rename_files.py

--- rename_files.py
+++ rename_files.py
@@ -4,13 +4,16 @@ def rename_files():
     #(1) get file name from a folder
     file_list = os.listdir(r"C:\Users\User\Documents\localdev\python-foundations-udacity\prank")
     #print(file_list)
-    save_path = os.getcwd()
+    saved_path = os.getcwd()
     print("Current Working Directory is "+saved_path)
     os.chdir(r"C:\Users\User\Documents\localdev\python-foundations-udacity\prank")
 
     #(2) for each file, rename filename
     for file_name in file_list:
-        os.rename(file_name, file_name.translate(None, "0123456789"))
+        print("Old Name: - "+file_name)
+        new_name = file_name.translate(None, "0123456789")
+        print("New Name: - "+new_name)
+        os.rename(file_name, new_name)
     os.chdir(saved_path)
 
 rename_files()
