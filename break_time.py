702f34fd62630b03eb18a573e82c2ba23f05ab23
André Ferreira <contato@andreferreira.me>
2017-09-18 13:47:20 -0300

    break_time e rename_files

---
+++ break_time.py
@@ -0,0 +1,11 @@
+import time
+import webbrowser
+
+breaks = 3
+break_count = 0
+
+print("This program started on "+time.ctime())
+while (break_count < breaks):
+    time.sleep(10)
+    webbrowser.open("https://www.andreferreira.me")
+    break_count = break_count + 1
