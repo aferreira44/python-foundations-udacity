a613633346bbaa322e764ce7261b770fdf470477
André Ferreira <contato@andreferreira.me>
2017-09-22 03:23:31 -0300

    Refactoring code of break_time.py

--- break_time.py
+++ break_time.py
@@ -4,8 +4,8 @@ import webbrowser
 breaks = 3
 break_count = 0
 
-print("This program started on "+time.ctime())
-while (break_count < breaks):
+print "This program started on " + time.ctime()
+while break_count < breaks:
     time.sleep(10)
     webbrowser.open("https://www.andreferreira.me")
     break_count = break_count + 1
